b49891dbb7385682503286e165446c9cad50f959
Sofia Catalan <scatalan96@gmail.com>
2020-10-19 18:24:34 -0500

    fixed minor bug that attempts to call cuda even for when running on cpu on engine.py, added command line args for automating tests across bayes, the beast, and local in torchTest.py

--- engine.py
+++ engine.py
@@ -84,7 +84,9 @@ def evaluate(model, data_loader, device):
     for images, targets in metric_logger.log_every(data_loader, 100, header):
         images = list(img.to(device) for img in images)
 
-        torch.cuda.synchronize()
+        if str(device)=='cuda':
+            torch.cuda.synchronize()
+        
         model_time = time.time()
         outputs = model(images)
 
--- torchTest.py
+++ torchTest.py
@@ -1,9 +1,8 @@
 # Sample code from the TorchVision 0.3 Object Detection Finetuning Tutorial
 # http://pytorch.org/tutorials/intermediate/torchvision_tutorial.html
 
-import sys
-
 import os
+import sys
 import numpy as np
 import torch
 from PIL import Image
@@ -16,6 +15,8 @@ from engine import train_one_epoch, evaluate
 import utils
 import transforms as T
 
+import time
+import datetime
 
 class PennFudanDataset(object):
     def __init__(self, root, transforms):
@@ -111,57 +112,124 @@ def get_transform(train):
     return T.Compose(transforms)
 
 
-def main():
-    # train on the GPU or on the CPU, if a GPU is not available
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+def configTestFromArgs(args):
 
-    # our dataset has two classes only - background and person
-    num_classes = 2
-    # use our dataset and defined transformations
-    dataset = PennFudanDataset('PennFudanPed', get_transform(train=True))
-    dataset_test = PennFudanDataset('PennFudanPed', get_transform(train=False))
-
-    # split the dataset in train and test set
-    indices = torch.randperm(len(dataset)).tolist()
-    dataset = torch.utils.data.Subset(dataset, indices[:-50])
-    dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
-
-    # define training and validation data loaders
-    data_loader = torch.utils.data.DataLoader(
-        dataset, batch_size=2, shuffle=True, num_workers=4,
-        collate_fn=utils.collate_fn)
-
-    data_loader_test = torch.utils.data.DataLoader(
-        dataset_test, batch_size=1, shuffle=False, num_workers=4,
-        collate_fn=utils.collate_fn)
-
-    # get the model using our helper function
-    model = get_model_instance_segmentation(num_classes)
-
-    # move model to the right device
-    model.to(device)
-
-    # construct an optimizer
-    params = [p for p in model.parameters() if p.requires_grad]
-    optimizer = torch.optim.SGD(params, lr=0.005,
-                                momentum=0.9, weight_decay=0.0005)
-    # and a learning rate scheduler
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
-                                                   step_size=3,
-                                                   gamma=0.1)
-
-    # let's train it for 10 epochs
-    num_epochs = 2
-
-    for epoch in range(num_epochs):
-        # train for one epoch, printing every 10 iterations
-        train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
-        # update the learning rate
-        lr_scheduler.step()
-        # evaluate on the test dataset
-        evaluate(model, data_loader_test, device=device)
-
-    print("That's it!")
+    # get initial time to estimate total run time of script
+    startTime = time.time()
     
+    if '-numTests' in args:
+        numTests = int(args[args.index('-numTests') + 1])
+    else:
+        numTests = 2
+
+    # configure device based on input arg string
+    if '-device' in args:
+        deviceStr = args[args.index('-device') + 1]
+        if (deviceStr=='gpu'):
+            if torch.cuda.is_available():
+                device = torch.device('cuda')
+            else:
+                print('user specified to run with gpu, but cuda is not available')
+                sys.exit()
+    else:
+        # if device is not specified, use cpu by default
+        device = torch.device('cpu')
+        deviceStr = 'cpu'
+
+    if '-outputFile' in args:
+        outputFilename = args[args.index('-outputFile') + 1]
+    else:
+        execTime = datetime.datetime.now()
+        execTime = str(execTime).replace(':', '-').replace(' ','_').split('.')[0]
+        print(execTime)
+        outputFilename = deviceStr + '_numTests' + str(numTests) + '_' + execTime + '.txt'
+        print(outputFilename)
+
+    return device, numTests, startTime, outputFilename
+
+def main(args):
+
+    # get user inputs
+    device, numTests, startTime, outputFilename = configTestFromArgs(args)
+
+    # sys.exit()
+    testTime = []
+    # testStartTime = 0 
+    # iterTestTime = 0 
+
+    for val in range(numTests):
+        testStartTime = time.time()
+
+        # our dataset has two classes only - background and person
+        num_classes = 2
+        # use our dataset and defined transformations
+        dataset = PennFudanDataset('PennFudanPed', get_transform(train=True))
+        dataset_test = PennFudanDataset('PennFudanPed', get_transform(train=False))
+
+        # split the dataset in train and test set
+        indices = torch.randperm(len(dataset)).tolist()
+        dataset = torch.utils.data.Subset(dataset, indices[:-50])
+        dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+
+        # define training and validation data loaders
+        data_loader = torch.utils.data.DataLoader(
+            dataset, batch_size=2, shuffle=True, num_workers=4,
+            collate_fn=utils.collate_fn)
+
+        data_loader_test = torch.utils.data.DataLoader(
+            dataset_test, batch_size=1, shuffle=False, num_workers=4,
+            collate_fn=utils.collate_fn)
+
+        # get the model using our helper function
+        model = get_model_instance_segmentation(num_classes)
+        
+        # move model to the right device
+        model.to(device)
+
+        # construct an optimizer
+        params = [p for p in model.parameters() if p.requires_grad]
+        optimizer = torch.optim.SGD(params, lr=0.005,
+                                    momentum=0.9, weight_decay=0.0005)
+        # and a learning rate scheduler
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                    step_size=3,
+                                                    gamma=0.1)
+
+        # let's train it for 2 epochs
+        num_epochs = 2
+
+        for epoch in range(num_epochs):
+            # train for one epoch, printing every 50 iterations
+            train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=50)
+            # update the learning rate
+            lr_scheduler.step()
+            # evaluate on the test dataset
+            evaluate(model, data_loader_test, device=device)
+
+        # save final model for later inspection 
+        torch.save(model.state_dict(), os.path.join('outputModels', 'test' + str(val) + '_' + outputFilename.replace('txt','pkl')))
+
+        iterTestTime = time.time() - testStartTime 
+        testTime.append(iterTestTime)
+
+    # Output time data
+    print('-----------------------------------')
+    print('PyTorch pedestrian detection test:')
+    print('    Device: ', str(device))
+    print('    Num tests: ', numTests)
+    print('    Mean training time: %f minutes' % (np.mean(testTime)/60.0))
+    print('    Stddev training time: %f minutes' % (np.std(testTime)/60.0))
+    print('    Script runtime: %f minutes' % ((time.time() - startTime)/60.0))
+
+    f = open(os.path.join('outputFiles', outputFilename), 'w')
+    f.write('PyTorch pedestrian detection test: \n')
+    f.write('    Device: ' + str(device) + '\n')
+    f.write('    Num tests: ' + str(numTests) + '\n')
+    f.write('    Mean training time: %f minutes' % (np.mean(testTime)/60.0) + '\n')
+    f.write('    Stddev training time: %f minutes' % (np.std(testTime)/60.0) + '\n')
+    f.write('    Script runtime: %f minutes' % ((time.time() - startTime)/60.0) + '\n')
+    f.close()
+    
+
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
